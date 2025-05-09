9e52c3d4e49d41e3b3fa6bb6e89dbdf3ed6c3616
jeessicats <jeessicats@gmail.com>
2025-05-09 18:01:23 -0300

    jQuery

--- section-19/jquery/index.js
+++ section-19/jquery/index.js
@@ -1,1 +1,9 @@
-$("h1").css("color", "blue");
+$("h1").addClass("big-title margin");
+$("h1").removeClass("big-title");
+$("h1").text("Goodbye");
+
+
+
+// document.querySelector("h1").style.color = "blue";
+$("button").css("background-color", "#4CAF50");
+$("button").css("color", "white");
