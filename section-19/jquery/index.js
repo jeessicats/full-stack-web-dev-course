89e77861f41434822be11b3c60345976e196fd7b
jeessicats <jeessicats@gmail.com>
2025-05-26 15:30:46 -0300

    updates

--- section-19/jquery/index.js
+++ section-19/jquery/index.js
@@ -1,7 +1,9 @@
 $("h1").addClass("big-title margin");
 $("h1").removeClass("big-title");
 $("h1").text("Goodbye");
+$("button").text("Don't click me");
 
+$("button").html("<em>Click</em>");
 
 
 // document.querySelector("h1").style.color = "blue";
---
+++ section-24/3.2 HTTP Requests/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  console.log(req.rawHeaders);
+  res.send("<h1> Home Page <h1>");
+});
+
+app.get("/about", (req, res) => {
+  res.send("<h1> About Me <h1>")
+});
+
+app.get("/contact", (req, res) => {
+  res.send("<h1> Contact Me <h1>")
+});
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+})
